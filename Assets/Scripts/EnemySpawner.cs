18fd936f828107c1bc0d066754f36c5faa6c0f34
Emanuel Maués da Costa Tavares <emanuelmaues@gmail.com>
2014-04-26 16:29:48 -0300

    Começo do Navmesh
    - Alguns aspectos do navmesh foram inseridos.
    - Já tem um cenário.
    - Já tem um inimigo que não faz nada.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : EnhancedBehaviour {
+
+	[SerializeField]
+	Enemy enemyPrefab;
+
+	/// <summary>
+	/// Guarda a posiçao das portas do jogo.
+	/// </summary>
+	[SerializeField]
+	Transform[] doors;
+
+	/// <summary>
+	/// The hero.
+	/// </summary>
+	[SerializeField]
+	Transform hero;
+
+	/// <summary>
+	/// The timer spawn.
+	/// </summary>
+	Timer timerSpawn;
+
+	/// <summary>
+	/// The spawn interval.
+	/// </summary>
+	float spawnInterval = 2f;
+
+	protected override void EnhancedStart ()
+	{
+		base.EnhancedStart ();
+
+		// Crio uma piscina de inimigos para reaproveitar
+		enemyPrefab.CreatePool();
+		InvokeRepeating("Spawn", Time.time, spawnInterval);
+	}
+	
+	void Spawn() {
+		Vector3 rndPosition = doors[Random.Range(0, doors.Length)].position;
+		Enemy enemy = enemyPrefab.Spawn(rndPosition);
+		enemy.Hero = hero;
+	}
+}
