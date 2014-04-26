72e5da67c7100875f32a29ebaa5608d0dd59d5d0
Emanuel Maués da Costa Tavares <emanuelmaues@gmail.com>
2014-04-26 16:48:25 -0300

    Merge com Jay

--- Assets/Scripts/SpherePowerUpTest.cs
+++ Assets/Scripts/SpherePowerUpTest.cs
@@ -31,6 +31,8 @@ public class SpherePowerUpTest : MonoBehaviour
 		foreach (Transform t in transform)
 		{
 			t.GetComponent<SphereManager>().moveSphere = true;
+			t.GetComponent<Animation>().Stop();
+			Destroy(t.gameObject, 2.0f);
 		}
 	}
 }
